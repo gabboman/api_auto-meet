4b44258cc7caa1868b9ea315b330aef5e48310c0
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2016-04-20 13:39:35 +0200

    instalado mysq, inicio de api

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(3000, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:3000/');
+var express = require('express');
+
+var app = express();
+
+var mysql      = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'user',
+  password : 'password',
+  database : 'database'
+});
+
+connection.connect();
+
+
+
+app.get('/wines', function(req, res) {
+    res.send([{name:'wine1'}, {name:'wine2'}]);
+});
+app.get('/wines/:id', function(req, res) {
+    res.send({id:req.params.id, name: "The Name", description: "description"});
+});
+
+app.listen(3000);
+console.log('Listening on port 3000...');
