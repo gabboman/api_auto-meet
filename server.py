69bbbc3165c613e32c005b227765422a247d1eb2
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2016-04-25 10:11:20 +0200

    Limpiando repo

--- server.py
+++ server.py
@@ -11,6 +11,8 @@ cursor = cnx.cursor()
 
 #inicializando:
 pueblos=dict()
+destinos=dict()
+
 query = ("SELECT * FROM pueblos "
          #"WHERE hire_date BETWEEN %s AND %s"
          )
@@ -20,12 +22,21 @@ cursor.execute(query)
 for (id_pueblo, nombre_pueblo, id_provincia) in cursor:
   pueblos[id_pueblo]=(nombre_pueblo,id_provincia)
 
+query = ("SELECT * FROM destinos"
+         #"WHERE hire_date BETWEEN %s AND %s"
+         )
+cursor.execute(query)
+for (id_destino,nombre_universidad,provincia) in cursor:
+    destinos[id_destino]=(nombre_universidad,provincia)
+
 cursor.close()
 cnx.close()
 @app.route("/<int:key1>/<int:key2>/", methods=['GET'])
 def getPueblo(key1,key2):
+    if key1 not in pueblos or key2 not in destinos:
+        return ["NOT FOUND"]
     (a,b)=pueblos[key1]
-    return {a:key2}
+    return {a:destinos[key2]}
 
 @app.route('/pueblos/')
 def example():
